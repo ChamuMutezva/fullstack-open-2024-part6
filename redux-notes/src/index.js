bb4838568e5d5c72ee1dd7a45d10ae8a91f6269c
Chamu Mutezva <ckmutezva@gmail.com>
2024-02-01 20:21:02 +0200

    part6 - notes tutorial

--- redux-notes/src/index.js
+++ redux-notes/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore, combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import App from "./App";
 import noteReducer from "./reducers/noteReducer";
@@ -8,12 +8,12 @@ import filterReducer from "./reducers/filterReducer";
 import { createNote } from "./reducers/noteReducer";
 import { filterChange } from "./reducers/filterReducer";
 
-const reducer = combineReducers({
-    notes: noteReducer,
-    filter: filterReducer,
+const store = configureStore({
+    reducer: {
+        notes: noteReducer,
+        filter: filterReducer,
+    },
 });
-
-const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 store.dispatch(filterChange("IMPORTANT"));
