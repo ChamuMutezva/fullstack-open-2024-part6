22b2f7282f02bb1ee4502e89b25968eddd1e662b
Chamu Mutezva <ckmutezva@gmail.com>
2024-02-02 06:17:14 +0200

    part6 - redux communication with the server - tutorial

--- redux-notes/src/index.js
+++ redux-notes/src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom/client";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import App from "./App";
-import noteReducer from "./reducers/noteReducer";
+import noteService from "./services/notes";
+import noteReducer, { setNotes} from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer";
-import { createNote } from "./reducers/noteReducer";
-import { filterChange } from "./reducers/filterReducer";
 
 const store = configureStore({
     reducer: {
@@ -14,12 +13,12 @@ const store = configureStore({
         filter: filterReducer,
     },
 });
-
-store.subscribe(() => console.log(store.getState()));
-store.dispatch(filterChange("IMPORTANT"));
-store.dispatch(
-    createNote("combineReducers forms one reducer from many simple reducers")
+/*
+noteService.getAll().then((notes) =>
+store.dispatch(setNotes(notes))
 );
+*/
+store.subscribe(() => console.log(store.getState()));
 
 console.log(store.getState());
 
