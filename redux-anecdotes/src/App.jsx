c9787965a4fc75155594edfc8ede9f55cc0c3f4c
Chamu Mutezva <ckmutezva@gmail.com>
2024-02-04 14:10:54 +0200

    part6 - ex 6.18 - voting redux thunk - updating votes to backend

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,19 +1,28 @@
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import { filterChange } from "./reducers/filterReducer";
 import Notification from "./components/Notification";
+import { initializeAnecdotes } from "./reducers/anecdoteReducer";
 
 const App = () => {
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(initializeAnecdotes());
+    }, []);
+
     const handleChange = (event) => {
         // input-field value is in variable event.target.value
         console.log(event.target.value);
         dispatch(filterChange(event.target.value));
     };
+
     const style = {
         marginBottom: 10,
     };
+
     return (
         <div>
             <h2>Anecdotes</h2>
--- redux-anecdotes/src/main.jsx
+++ redux-anecdotes/src/main.jsx
@@ -2,15 +2,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./store";
-import anecdoteService from "./services/anecdotes";
-import  { appendAnecdote } from "./reducers/anecdoteReducer";
 
-
-anecdoteService.getAll().then(anecdotes => {
-    anecdotes.forEach(anecdote => {
-        store.dispatch(appendAnecdote(anecdote))
-    })
-})
 console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
